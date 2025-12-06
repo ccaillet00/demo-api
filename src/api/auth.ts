e9266e23deaff2991ef8fc8507b219ac702e9487
ccaillet00 <ccaillet@swissonline.ch>
2025-12-06 13:41:30 +0100

    login endpoint created

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,8 +2,11 @@ import { type Request, type Response, type Express } from "express"
 import bcrypt from "bcrypt"
 import { userTable } from "../db/schema"
 import { db } from "../db/database"
+import { eq } from "drizzle-orm"
 
 export const iniializeAuthAPI = (app: Express) => {
+
+const jwtSecret = process.env.JWT_SECRET ||
 
     app.post("/api/auth/register", async (req: Request, res: Response) => {
         const { username, password } = req.body;
@@ -12,5 +15,19 @@ export const iniializeAuthAPI = (app: Express) => {
 
         const newUser = await db.insert(userTable).values({username: username, password: hashedPassword})
         res.send({ message: "User registered successfully", user: newUser });
+    })
+
+    app.post("/api/auth/login", async (req: Request, res: Response) => {
+        const { username, password } = req.body;
+        const user = await db.select().from(userTable).where(eq(userTable.username, username)).limit(1);
+        const checkPassword = user[0] ? bcrypt.compareSync(password, user[0].password) : false;
+    
+        if (!checkPassword) {
+            return res.status(401).send({ message: "Invalid credentials" });
+        }
+        else {
+            return res.send({ message: "Login successful", user: user[0] });
+        }
+
     })
 }
