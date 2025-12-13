4f7ca15f4a19c3970fe7f17f0a0bc95ed926c15c
ccaillet00 <ccaillet@swissonline.ch>
2025-12-13 11:20:40 +0100

    initialize frontend

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { initializeAPI } from "./api/index";
+import cors from "cors";
 
 const app = express()
 app.use(express.json());
+app.use(cors());
 const port = 3000
 
 initializeAPI(app)
